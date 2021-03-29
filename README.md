74bb6899b958c8b3e765c0fb082c997bb924e58c
Jay <jaylimcode@gmail.com>
2021-03-30 04:44:32 +0900

    Add break feature

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Pycraft
 -----------------
 Python Minecraft using ursina engine
 <img width="624" alt="screenshot" src="https://user-images.githubusercontent.com/59774709/111787053-d5cebb00-8901-11eb-834f-10d8c8f20378.png">
-
+<img width="624" alt="screenshot" src="https://user-images.githubusercontent.com/59774709/112890459-dd683e00-9111-11eb-97a0-0b02f38fe10e.png">
 Assets
 ----------------
 + I made the 3D textures and models
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 app = Ursina()
 
 
-item_pick = 0
+block_pick = 0
 window.title = 'Pycraft'
 window.fullscreen = True
 window.color = color.color(180, 50, 80) # sky hsb color
@@ -12,38 +12,51 @@ window.color = color.color(180, 50, 80) # sky hsb color
 punch = Audio('assets/sound/punch.wav', autoplay=False)
 
 items = [
-    load_texture('assets/texture/arm_texture.png'),  #0 hand
+    [
+        load_texture('assets/texture/arm_texture.png')  #0 hand
+    ],
+    
+    [
     load_texture('assets/texture/grass_block/grass_block.png'),  # 1 grass
     load_texture('assets/texture/grass_block/grass_block_break1.png'),
     load_texture('assets/texture/grass_block/grass_block_break2.png'),
     load_texture('assets/texture/grass_block/grass_block_break3.png'),
     load_texture('assets/texture/grass_block/grass_block_break4.png'),
-    load_texture('assets/texture/grass_block/grass_block_break5.png'),  
+    load_texture('assets/texture/grass_block/grass_block_break5.png') 
+    ],
 
-    load_texture('assets/texture/stone_block/stone_block.png'),   # 7
-    load_texture('assets/texture/stone_block/stone_block_break1.png'),
-    load_texture('assets/texture/stone_block/stone_block_break2.png'),
-    load_texture('assets/texture/stone_block/stone_block_break3.png'),
-    load_texture('assets/texture/stone_block/stone_block_break4.png'),
-    load_texture('assets/texture/stone_block/stone_block_break5.png'),  
+    [
+        load_texture('assets/texture/stone_block/stone_block.png'),   # 2 stone
+        load_texture('assets/texture/stone_block/stone_block_break1.png'),
+        load_texture('assets/texture/stone_block/stone_block_break2.png'),
+        load_texture('assets/texture/stone_block/stone_block_break3.png'),
+        load_texture('assets/texture/stone_block/stone_block_break4.png'),
+        load_texture('assets/texture/stone_block/stone_block_break5.png')  
+    ],
 
-    load_texture('assets/texture/brick_block/brick_block.png'),    # 13
-    load_texture('assets/texture/brick_block/brick_block_break1.png'),
-    load_texture('assets/texture/brick_block/brick_block_break2.png'),
-    load_texture('assets/texture/brick_block/brick_block_break3.png'),
-    load_texture('assets/texture/brick_block/brick_block_break4.png'),
-    load_texture('assets/texture/brick_block/brick_block_break5.png'),
+    [
+        load_texture('assets/texture/brick_block/brick_block.png'),    # 3 brick
+        load_texture('assets/texture/brick_block/brick_block_break1.png'),
+        load_texture('assets/texture/brick_block/brick_block_break2.png'),
+        load_texture('assets/texture/brick_block/brick_block_break3.png'),
+        load_texture('assets/texture/brick_block/brick_block_break4.png'),
+        load_texture('assets/texture/brick_block/brick_block_break5.png')
+    ],
 
-    load_texture('assets/texture/dirt_block/dirt_block.png'),  # 19
-    load_texture('assets/texture/dirt_block/dirt_block_break1.png'),
-    load_texture('assets/texture/dirt_block/dirt_block_break2.png'),
-    load_texture('assets/texture/dirt_block/dirt_block_break3.png'),
-    load_texture('assets/texture/dirt_block/dirt_block_break4.png'),
-    load_texture('assets/texture/dirt_block/dirt_block_break5.png'),  
+    [
+        load_texture('assets/texture/dirt_block/dirt_block.png'),  # 4 dirt
+        load_texture('assets/texture/dirt_block/dirt_block_break1.png'),
+        load_texture('assets/texture/dirt_block/dirt_block_break2.png'),
+        load_texture('assets/texture/dirt_block/dirt_block_break3.png'),
+        load_texture('assets/texture/dirt_block/dirt_block_break4.png'),
+        load_texture('assets/texture/dirt_block/dirt_block_break5.png')  
+    ]
 ]
 
+
+
 def update():
-    global item_pick
+    global block_pick
 
     if held_keys['left mouse'] or held_keys['right mouse']:
         punch.play()
@@ -51,16 +64,18 @@ def update():
     else:
         hand.passive() 
 
-    if held_keys['0']: item_pick = 0 # hand
-    if held_keys['1']: item_pick = 1
-    if held_keys['2']: item_pick = 7
-    if held_keys['3']: item_pick = 13
-    if held_keys['4']: item_pick = 19
+    if held_keys['0']: block_pick = 0 # hand
+    if held_keys['1']: block_pick = 1
+    if held_keys['2']: block_pick = 2
+    if held_keys['3']: block_pick = 3
+    if held_keys['4']: block_pick = 4
     
     hand.item_held()
 
 class Voxel(Button):
-    def __init__(self, position = (0,0,0), texture = items[1]):
+    def __init__(self, position = (0,0,0), texture = items[1][0]):
+        self.block_id = 1
+        self.block_health = 100
         super().__init__(
             parent = scene,
             position = position,
@@ -71,37 +86,54 @@ class Voxel(Button):
             highlight_color = color.white,
             scale = 0.5
         )
+            
 
     def input(self, key):
         if self.hovered:
             if key == 'right mouse down':
-                if item_pick == 1: voxel = Voxel(position = self.position + mouse.normal, texture = items[item_pick])
-                if item_pick == 7: voxel = Voxel(position = self.position + mouse.normal, texture = items[item_pick])
-                if item_pick == 13: voxel = Voxel(position = self.position + mouse.normal, texture = items[item_pick])
-                if item_pick == 19: voxel = Voxel(position = self.position + mouse.normal, texture = items[item_pick])
+                if block_pick == 1: voxel = Voxel(position = self.position + mouse.normal, texture = items[block_pick][0]) # items[ block id][nomal block texture]
+                if block_pick == 2: voxel = Voxel(position = self.position + mouse.normal, texture = items[block_pick][0])
+                if block_pick == 3: voxel = Voxel(position = self.position + mouse.normal, texture = items[block_pick][0])
+                if block_pick == 4: voxel = Voxel(position = self.position + mouse.normal, texture = items[block_pick][0])
+                self.block_id = block_pick
 
             if key == 'left mouse down':
-                destroy(self)
+                self.block_health -= 16
+                if self.block_health <= 5:
+                    destroy(self)
+                elif self.block_health <= 20:
+                    self.texture = items[self.block_id][5]
+                elif self.block_health <= 40:
+                    self.texture = items[self.block_id][4]
+                elif self.block_health <= 55:
+                    self.texture = items[self.block_id][3]
+                elif self.block_health <= 70:
+                    self.texture = items[self.block_id][2]
+                elif self.block_health <= 85:
+                    self.texture = items[self.block_id][1]
+                elif self.block_health == 100:
+                    self.texture = items[self.block_id][0]
+                    
 
 class Hand(Entity):
     def __init__(self):
         super().__init__(
             parent = camera.ui,
             model = 'assets/model/arm.obj',
-            texture = items[0],
+            texture = items[0][0],
             scale = 0.07,
             rotation = Vec3(0, 70, 20),
             position = Vec2(0.8, -0.5)
         )
 
     def item_held(self):  # switching item for held ui
-        if (item_pick == 0):
+        if (block_pick == 0):
             self.model = 'assets/model/arm.obj'
-            self.texture = items[0]
+            self.texture = items[0][0]
             self.scale = 0.07
         else:
             self.model = 'assets/model/block.obj'
-            self.texture = items[item_pick]
+            self.texture = items[block_pick][0]
             self.scale = 0.1
 
     def active(self):
